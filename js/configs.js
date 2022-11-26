c0644f2b58d93bc2fd8773341482dd32051f60f3
Darkynsky <telles861@gmail.com>
2022-11-26 20:52:34 -0300

    javascript menu elementais

---
+++ js/configs.js
@@ -0,0 +1,246 @@
+var buttonToxina = document.querySelector("#btnToxina");
+var buttonGlacial = document.querySelector("#btnGlacial");
+var buttonEletrico = document.querySelector("#btnEletrico");
+var buttonRadiacao = document.querySelector("#btnRadiacao");
+var buttonMagnetismo = document.querySelector("#btnMagnetismo");
+var buttonIgnio = document.querySelector("#btnIgnio");
+var buttonGasoso = document.querySelector("#btnGasoso");
+var buttonCorrosivo = document.querySelector("#btnCorrosivo");
+var buttonViral = document.querySelector("#btnViral");
+
+var conteudoToxina = document.querySelector("#conteudoToxina");
+var conteudoGlacial = document.querySelector("#conteudoGlacial");
+var conteudoEletrico = document.querySelector("#conteudoEletrico");
+var conteudoRadiacao = document.querySelector("#conteudoRadiacao");
+var conteudoMagnestismo = document.querySelector("#conteudoMagnetismo");
+var conteudoIgnio = document.querySelector("#conteudoIgnio");
+var conteudoGasoso = document.querySelector("#conteudoGasoso");
+var conteudoCorrosivo = document.querySelector("#conteudoCorrosivo");
+var conteudoViral = document.querySelector("#conteudoViral");
+
+
+buttonToxina.addEventListener('click', function () {
+
+    if (conteudoToxina.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoToxina.style.display = 'block';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonGlacial.addEventListener('click', function () {
+
+    if (conteudoGlacial.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoGlacial.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonEletrico.addEventListener('click', function () {
+
+    if (conteudoEletrico.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoEletrico.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonRadiacao.addEventListener('click', function () {
+
+    if (conteudoRadiacao.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoRadiacao.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonMagnetismo.addEventListener('click', function () {
+
+    if (conteudoMagnestismo.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoMagnestismo.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonIgnio.addEventListener('click', function () {
+
+    if (conteudoIgnio.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoIgnio.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonGasoso.addEventListener('click', function () {
+
+    if (conteudoGasoso.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoGasoso.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonCorrosivo.addEventListener('click', function () {
+
+    if (conteudoCorrosivo.style.display === 'block') {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    } else {
+        conteudoCorrosivo.style.display = 'block';
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoViral.style.display = 'none'
+    }
+});
+
+buttonViral.addEventListener('click', function () {
+
+    if (conteudoViral.style.display === 'block') {
+        conteudoViral.style.display = 'none'
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+    } else {
+        conteudoToxina.style.display = 'none';
+        conteudoGlacial.style.display = 'none';
+        conteudoEletrico.style.display = 'none';
+        conteudoRadiacao.style.display = 'none';
+        conteudoMagnestismo.style.display = 'none';
+        conteudoIgnio.style.display = 'none';
+        conteudoGasoso.style.display = 'none';
+        conteudoCorrosivo.style.display = 'none';
+        conteudoViral.style.display = 'block'
+    }
+});
+/* botoes do conteudo termina*/
